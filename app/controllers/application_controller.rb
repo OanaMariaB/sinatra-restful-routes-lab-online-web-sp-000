67253338addc8eb08969fba801028459fa8c2b70
Oana Berentan <oana.berentan@yahoo.com>
2020-02-08 20:25:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,21 +9,19 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/recipes/new' do
-    erb :new
-  end
-
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect to '/recipes/#{@recipe.id}'
+  end
+
+  get '/recipes/new' do
+    erb :new
   end
 
   get '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
     erb :show
   end
-
-
 
   get '/recipes/:id/edit' do
     @recipe = Recipe.find(params[:id])
