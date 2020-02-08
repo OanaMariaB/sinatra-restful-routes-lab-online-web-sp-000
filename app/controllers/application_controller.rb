ea859295b74638a053521d75d73f7143a11dfd81
Oana Berentan <oana.berentan@yahoo.com>
2020-02-08 20:11:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,11 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to '/recipes/#{@recipe.id}'
+  end
+
 
 
 
